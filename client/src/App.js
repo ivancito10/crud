fcdc23b8211d394f60f4c6bba2f1b32a67e03e05
ivancito10 <ivanphilco10@gmail.com>
2024-10-09 12:02:47 -0400

    Se agrega el proyecto con inicio del formulario

---
+++ client/src/App.js
@@ -0,0 +1,18 @@
+
+import './App.css';
+
+function App() {
+  return (
+    <div className="App">
+      <div className= "datos">
+        <label>Nombre: <input type="text"></input></label><br/>
+        <label>Edad: <input type="text"></input></label><br/>
+        <label>Pais: <input type="text"></input></label><br/>
+        <label>Cargo: <input type="text"></input></label><br/>
+        <label>Años: <input type="text"></input></label><br/>
+      </div>
+    </div>
+  );
+}
+
+export default App;
