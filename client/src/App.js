6e394aec03494d6fb60aa5edc986fcb18116cdeb
ivancito10 <ivanphilco10@gmail.com>
2024-10-09 12:36:27 -0400

    Creando constantes para el formulario

--- client/src/App.js
+++ client/src/App.js
@@ -1,15 +1,25 @@
 
 import './App.css';
-
+import { useState } from "react"
 function App() {
+  const [nombre,setNombre] = useState("");
+  const [edad,setEdad] = useState(0);
+  const [pais,setPais] = useState("");
+  const [cargo,setCargo] = useState("");
+  const [anios,setAnios] = useState(0);
   return (
     <div className="App">
       <div className= "datos">
-        <label>Nombre: <input type="text"></input></label><br/>
-        <label>Edad: <input type="text"></input></label><br/>
-        <label>Pais: <input type="text"></input></label><br/>
-        <label>Cargo: <input type="text"></input></label><br/>
-        <label>Años: <input type="text"></input></label><br/>
+        <label>Nombre: <input 
+        onChange={(event)=>{
+          setNombre(event.target.value);
+        }}
+        type="text"/></label>
+        <label>Edad: <input type="number"/></label>
+        <label>Pais: <input type="text"/></label>
+        <label>Cargo: <input type="text"/></label>
+        <label>Años: <input type="number"/></label>
+        <button>Registrar</button>
       </div>
     </div>
   );
